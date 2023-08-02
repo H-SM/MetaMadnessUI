64060bf2aca046046e3c5a5f82f8ef0c39ea8830
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-08-02 06:19:15 +0530

    added hero section to the UI

--- project/components/Navbar.jsx
+++ project/components/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => (
       <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>  
         METAVERSUS
       </h2>
+      <img src="/menu.svg" alt="menu" className='w-[24px] h-[24px] object-contain'/>
     </div>  
   </motion.nav>
 );
